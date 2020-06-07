14e9abad8c961f5f788e4b194d57c94300aaded4
davidamarcosp <davidamarcosp@gmail.com>
2020-06-07 15:19:44 -0300

    Updates

---
+++ precache-manifest.2e7ec72752da721ab17f91afb5508c8f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2b8bf2844365c1fe99857cfd1ca049",
+    "url": "/boosting-site/index.html"
+  },
+  {
+    "revision": "dfc4cbbacff0f8378a4c",
+    "url": "/boosting-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecec0a9b9809a63808cd",
+    "url": "/boosting-site/static/js/2.d0b06690.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/boosting-site/static/js/2.d0b06690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc4cbbacff0f8378a4c",
+    "url": "/boosting-site/static/js/main.62ef3155.chunk.js"
+  },
+  {
+    "revision": "eea81a55aca6b832a502",
+    "url": "/boosting-site/static/js/runtime-main.94275756.js"
+  },
+  {
+    "revision": "d65e0b387b5de5735cfb05f4c7e1c5a0",
+    "url": "/boosting-site/static/media/Banner.d65e0b38.jpg"
+  },
+  {
+    "revision": "f09e4946dd417667a9e710e727867ba4",
+    "url": "/boosting-site/static/media/Chat.f09e4946.png"
+  },
+  {
+    "revision": "61047e80866c994782335d1eecb60f6b",
+    "url": "/boosting-site/static/media/Logo.61047e80.jpg"
+  },
+  {
+    "revision": "9b451a496816572ac70bb008940fc3cc",
+    "url": "/boosting-site/static/media/Match.9b451a49.png"
+  },
+  {
+    "revision": "286242f0d1d5d91fd881c9fd17aff5a0",
+    "url": "/boosting-site/static/media/Roles.286242f0.jpg"
+  }
+]);
